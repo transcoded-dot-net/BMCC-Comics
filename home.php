4f72cc4dd4c6393aa5ad7c5fd89e3b228526360c
Elle <116987903+urmomstoaster@users.noreply.github.com>
2023-11-16 11:32:40 -0500

    init commit

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+?>
+<html lang="en">
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }</style>
+<body>
+<h1>BMCC Comics Collector</h1>
+<ul>
+    <li><a href="default.asp">Home</a></li>
+    <li><a href="news.asp">News</a></li>
+    <li><a href="contact.asp">Contact</a></li>
+    <li><a href="about.asp">About</a></li>
+</ul>
+
+</body>
+</html>
